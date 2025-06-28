8542c86593e65dcdd3a4d3ceb1ce1294f6db4585
Bird <you@example.co>
2025-06-28 09:59:33 +0200

    Added Different Charsets

--- main.nim
+++ main.nim
@@ -1,23 +1,53 @@
 import std/random
 import std/strutils
 
-proc generate(length: int) =
+proc option(question: string): bool =
+    echo r"[y/N] ", question
+    var yorn: string = readLine(stdin)
+    if yorn == "y":
+        return true
+    elif yorn == "y":
+        return true
+    else:
+        return false
+
+proc generate(length: int,specialchar: bool,numberchar:bool) =
     stdout.write "Password: "
     for i in countup(1,length):
-        let characterset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-        let pick = sample(characterset)
-        doAssert pick in characterset
-        stdout.write pick
+        if specialchar:
+            let specialcharacterset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","!","?","$","%","&","=","-","_","#","'","*","+"]
+            let pick = sample(specialcharacterset)
+            doAssert pick in specialcharacterset
+            stdout.write pick
+        if numberchar:
+            let numbercharacterset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","1","2","3","4","5"]
+            let pick = sample(numbercharacterset)
+            doAssert pick in numbercharacterset
+            stdout.write pick
+        if numberchar and specialchar:
+            let specialnumberchatset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","!","?","$","%","&","=","-","_","#","'","*","+"]
+            let pick = sample(specialnumberchatset)
+            doAssert pick in specialnumberchatset
+            stdout.write pick            
+        else:
+            let characterset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+            let pick = sample(characterset)
+            doAssert pick in characterset
+            stdout.write pick
     echo ""
 #-- Main ->
 
 echo "How Long?"
 var length: string = readLine(stdin)
+var specialchar: bool = option("Use Special Characters?")
+var numberchar: bool = option("Use Numbers?")
+
+
 randomize()
 
 try:
     var length_int = parseInt(length)
-    generate(length_int)
+    generate(length_int,specialchar,numberchar)
 
 except ValueError:
-    echo "\nInvalid Type Provided\nPlease input a Number!"
+    echo "\nInvalid Type Provided for Length\nPlease input a Number!"
