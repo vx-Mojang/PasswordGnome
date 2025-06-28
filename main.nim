b56789e1110fc2eb3f2fca79ddb90d651a7fe09c
Bird <you@example.co>
2025-06-28 07:51:49 +0200

    Counting Demo

--- main.nim
+++ main.nim
@@ -1,1 +1,20 @@
-echo "Hello world"
+import std/random
+import std/strutils
+
+proc counting(length: int) =
+    echo "\n"
+    for i in countup(1,length):
+        
+        echo "Toast x",i,"\r"
+
+#-- Main ->
+
+echo "How Much?"
+randomize()
+
+var length: string = readLine(stdin)
+try:
+    var length_int = parseInt(length)
+    counting(length_int)
+except ValueError:
+    echo "\nInvalid Type Provided\nPlease input a Number!"
